219277f25781ab0f1e04af5f6674934569187d0f
Sallai András <andrewteki@gmail.com>
2026-02-08 10:42:36 +0100

    Fixed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 export default {
   server: {
     port: 3000
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        calc: 'calc.html',
+        about: 'about.html'
+      }
+    }
   }
 }
